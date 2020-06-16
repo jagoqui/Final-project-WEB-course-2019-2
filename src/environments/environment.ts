31be37ee956a65e5d4d33ad3cdd4f537a6f2c2c5
= <=>
2020-06-16 15:06:36 -0500

    Instalacion de depedencias nativamente : Bootstrap | jquery | poppers | ngx-spinner | Bootwatch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAObnASPbZEtCzREPj59JZQe4cb4QjseW0',
+    authDomain: 'access-control-system-jaidiver.firebaseapp.com',
+    databaseURL: 'https://access-control-system-jaidiver.firebaseio.com',
+    projectId: 'access-control-system-jaidiver',
+    storageBucket: 'access-control-system-jaidiver.appspot.com',
+    messagingSenderId: '612985677494',
+    appId: '1:612985677494:web:8c9645a2fee2f577392817'
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
