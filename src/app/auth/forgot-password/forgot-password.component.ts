ced45b3867abc54c30652361bfcaf0a48a85e446
Yenny Hernandez <yeni.hernandez@udea.edu.co>
2020-07-14 19:54:55 -0500

    Instalación de Angular material, se necesita hacer un merge

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from '@auth/services/auth.service';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import SwAlert from 'sweetalert2';
 
 @Component({
   selector: 'app-forgot-password',
@@ -10,13 +11,39 @@ import { Router } from '@angular/router';
 })
 export class ForgotPasswordComponent {
   userEmail = new FormControl('');
-  constructor(private authSvc: AuthService, private router: Router) {}
+  constructor(private authSvc: AuthService, private router: Router) { }
 
   async onReset() {
     try {
       const email = this.userEmail.value;
       await this.authSvc.resetPassword(email);
-      window.alert('Email sent, check your inbox!');
+      let timerInterval
+      SwAlert.fire({
+        title: 'Auto close alert!',
+        html: 'I will close in <b></b> milliseconds.',
+        timer: 2000,
+        timerProgressBar: true,
+        onBeforeOpen: () => {
+          SwAlert.showLoading()
+          timerInterval = setInterval(() => {
+            const content = SwAlert.getContent()
+            if (content) {
+              const b = content.querySelector('b')
+              if (b) {
+                b.textContent += SwAlert.getTimerLeft()
+              }
+            }
+          }, 100)
+        },
+        onClose: () => {
+          clearInterval(timerInterval)
+        }
+      }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === SwAlert.DismissReason.timer) {
+          console.log('I was closed by the timer')
+        }
+      })
       this.router.navigate(['/login']);
     } catch (error) {
       console.log(error);
